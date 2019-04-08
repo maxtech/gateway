13c0b4df09e7998ffe453346abeeb68f362f8e6d
Maximilian Wong <maximilian520@163.com>
2019-04-08 14:57:28 +0800

    update gateway

--- websocket/client.go
+++ websocket/client.go
@@ -40,7 +40,7 @@ func (c *Client) readPump() {
         _ = c.conn.Close()
     }()
     c.conn.SetReadLimit(maxMessageSize)
-    c.conn.SetReadDeadline(time.Now().Add(pongWait))
+    _ = c.conn.SetReadDeadline(time.Now().Add(pongWait))
     c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
     for {
         _, message, err := c.conn.ReadMessage()
@@ -83,15 +83,15 @@ func (c *Client) writePump() {
     ticker := time.NewTicker(pingPeriod)
     defer func() {
         ticker.Stop()
-        c.conn.Close()
+        _ = c.conn.Close()
     }()
     for {
         select {
         case message, ok := <-c.send:
-            c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+            _ = c.conn.SetWriteDeadline(time.Now().Add(writeWait))
             if !ok {
                 // The hub closed the channel.
-                c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+                _ = c.conn.WriteMessage(websocket.CloseMessage, []byte{})
                 return
             }
 
@@ -99,20 +99,20 @@ func (c *Client) writePump() {
             if err != nil {
                 return
             }
-            w.Write(message)
+            _, _ = w.Write(message)
 
             // Add queued chat messages to the current websocket message.
             n := len(c.send)
             for i := 0; i < n; i++ {
-                w.Write(newline)
-                w.Write(<-c.send)
+                _, _ = w.Write(newline)
+                _, _ = w.Write(<-c.send)
             }
 
             if err := w.Close(); err != nil {
                 return
             }
         case <-ticker.C:
-            c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+            _ = c.conn.SetWriteDeadline(time.Now().Add(writeWait))
             if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
                 return
             }
