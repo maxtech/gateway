4a7b8c7677ca77e50205605a819ea0cce96c51d6
Maximilian <maximilian520@163.com>
2019-08-25 19:27:36 +0800

    update mux

--- http_gateway/mux/server_util.go
+++ http_gateway/mux/server_util.go
@@ -19,7 +19,7 @@ type serverUtil struct {
 
 var ServerUtil *serverUtil
 
-func (s *serverUtil) InitMux(mode string) *mux.Router {
+func (s *serverUtil) InitMux() *mux.Router {
     return mux.NewRouter()
 }
 
