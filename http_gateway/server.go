d086cd51179470fdd3cc4c6126cbc8d37fb3e686
Maximilian <maximilian520@163.com>
2019-08-25 18:26:59 +0800

    update

--- http_gateway/server.go
+++ http_gateway/server.go
@@ -16,12 +16,29 @@ func (s *serverUtil) Init(mode string) *gin.Engine {
     return gin.Default()
 }
 
-func (s *serverUtil) Start(engine *gin.Engine, https bool, address, certFile, keyFile string) {
+func (s *serverUtil) Start(mode string, https bool, address, certFile, keyFile string) {
+    gin.SetMode(mode)
+    engine := gin.Default()
     var err error
     if https {
         err = http.ListenAndServeTLS(address, certFile, keyFile, engine)
     } else {
         err = http.ListenAndServe(address, engine)
+    }
+    if err != nil {
+        log.Fatal(err)
+    }
+    return
+}
+
+func (s *serverUtil) StartByConfig(mode string, httpConfigFormat HttpConfigFormat) {
+    var err error
+    gin.SetMode(mode)
+    engine := gin.Default()
+    if httpConfigFormat.Https {
+        err = http.ListenAndServeTLS(httpConfigFormat.Address, httpConfigFormat.CertFile, httpConfigFormat.KeyFile, engine)
+    } else {
+        err = http.ListenAndServe(httpConfigFormat.Address, engine)
     }
     if err != nil {
         log.Fatal(err)
